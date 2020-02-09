0d2ac1af650a3f81345fa7aacdf0172427f13ab5
Berat Uslu <berat.uslu.me@gmail.com>
2020-02-09 22:26:23 +0300

    cors allowed

--- server/server.js
+++ server/server.js
@@ -1,44 +1,54 @@
-'use strict';
+"use strict";
 /**
  * Require our modules
  */
-const express = require('express');
+const express = require("express");
 // Set the App
 const app = express();
 // Require our Main API Router
-const apiV1 = require('./apiV1/router');
+const apiV1 = require("./apiV1/router");
 
-const healthcheck = require('./healthcheck/healthcheck');
-const login = require('./authentication/login/login');
-const verify = require('./authentication/verify/verify');
-const caching = require('./caching/caching');
+//Require cors
+const cors = require("cors");
+
+const healthcheck = require("./healthcheck/healthcheck");
+const login = require("./authentication/login/login");
+const verify = require("./authentication/verify/verify");
+const caching = require("./caching/caching");
 // Require our Global Middleware
-require('./middleware/middleware')(app);
+require("./middleware/middleware")(app);
 
 /**
  * Generic API's
  */
 
 // Use protected Endpoints
-app.use('/*', require( './authentication/protected-endpoints/protected-enpoints' ));
+
+//Enable cors
+app.use(cors());
+
+app.use(
+  "/*",
+  require("./authentication/protected-endpoints/protected-enpoints")
+);
 
 // Login
-app.use('/login', login);
+app.use("/login", login);
 
 // Verify Login
-app.use('/login/verify', verify);
+app.use("/login/verify", verify);
 
 // Healthcheck
-app.use('/healthcheck', healthcheck);
+app.use("/healthcheck", healthcheck);
 
 // Caching
-app.use('/cache', caching);
+app.use("/cache", caching);
 
 // Add all the API - Version 1
-app.use('/api/v1', apiV1);
+app.use("/api/v1", apiV1);
 
 // Error Handler
-require('./errorhandler/errorhandler')(app);
+require("./errorhandler/errorhandler")(app);
 
 // Export the app
 module.exports = app;
